b044eef4a7718e5c87080ddec0f571a14fdd2e2a
gulbuilds <280878262+gulbuilds@users.noreply.github.com>
2026-06-24 06:54:23 +0500

    add Variables and data types

---
+++ javascript/1-variables-datatypes.js
@@ -0,0 +1,25 @@
+// variables let , const , var 
+
+let mode= "sad";    // let is used when value will cahange 
+ mode=  "happy";      //let allows reassignment, but not redeclaration in the same scope
+
+var oldStyleVariable= "avoid it";
+
+// Primitive  datatypes
+const name = "Gul";  
+const age= 123;    
+const country = "Pakistan";      // string in double quotes
+const loggedin = true;         // boolean 
+let city = undefined;         // declared but no value assign yet 
+let area = null;             // empty value  (intentionally set)
+
+// check the type
+console.log(typeof age);        // numder
+console.log(typeof loggedin);   // boolean 
+// print in screen
+console.log("User Name:",name);       //Gul
+console.log("Current mode of user:",mode);    //happy
+
+
+
+
